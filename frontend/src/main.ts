dd8e804dc5c473c590ba322aac6a456071aadcc3
Brunogmuniz <devbrunomuniz@gmail.com>
2026-01-31 15:58:25 -0300

    Ajuste da versão do angular

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,6 @@
-import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { NbThemeModule, NbLayoutModule, NbIconModule } from '@nebular/theme';
-
-bootstrapApplication(App, {
-  providers: [
-    provideAnimations(),
-    ...(NbThemeModule.forRoot({ name: 'default' }).providers || []),
-    NbLayoutModule,
-    NbIconModule
-  ]
-})
-  .catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
